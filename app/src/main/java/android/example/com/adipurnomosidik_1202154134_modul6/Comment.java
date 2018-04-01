0cae6cf81bc6c8e904d0aad1ac455e935283a09d
Adi Purnomo <adipurnomosidik97@gmail.com>
2018-04-01 23:12:24 +0700

    push pertama kali

---
+++ app/src/main/java/android/example/com/adipurnomosidik_1202154134_modul6/Comment.java
@@ -0,0 +1,32 @@
+package android.example.com.adipurnomosidik_1202154134_modul6;
+
+
+public class Comment {
+    String postId;
+    String username;
+    String message;
+
+    public String getPostId() {
+        return postId;
+    }
+
+    public void setPostId(String postId) {
+        this.postId = postId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
